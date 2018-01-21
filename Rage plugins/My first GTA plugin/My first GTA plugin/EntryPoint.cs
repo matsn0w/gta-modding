fac244942e809152d8cc4ef33d1dd2ac311aef80
matsn0w <bartscholtus@gmail.com>
2018-01-21 15:07:12 +0100

    Started with GTA Rage scripting!

---
+++ Rage plugins/My first GTA plugin/My first GTA plugin/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+
+[assembly: Rage.Attributes.Plugin("My First LSPDFR Plugin", Description = "This is my first plugin. yay!", Author = "matsn0w")]
+
+namespace My_first_GTA_plugin
+{
+    public static class EntryPoint
+    {
+        public static void Main()
+        {
+            GameFiber.StartNew(delegate
+            {
+                Game.DisplayHelp("Hello Bart, how are you doing today?");
+                Game.DisplayNotification("Hello to the TeamMOH Discord!!!");
+            });
+        }
+    }
+}
